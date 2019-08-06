2bd21fa6f7322966a4cb7945e5cfcd13d73c1acd
moinultm <moinultm@gmail.com>
2019-08-06 23:21:26 +0600

    pagination soleved  param  header

--- src/app/services/common/crud.service.ts
+++ src/app/services/common/crud.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { TokenService } from '../security/token.service';
-import { HttpClient,HttpParams} from '@angular/common/http';
+import { HttpClient,HttpParams, HttpHeaders} from '@angular/common/http';
 import { PartialList } from 'src/app/models/common/patial-list.model';
 import { Role } from 'src/app/models/role.model';
 
@@ -15,20 +15,20 @@ export class CrudService<T> {
   list : PartialList<T>;
 
   constructor(private _http:  HttpClient  ) {
-    this.headers = new Headers({ 
-      'Accept': 'application/json', 
-      'Content-Type': 'application/json',
-      'Cache-Control': 'no-cache, no-store, must-revalidate, post-check=0, pre-check=0',
-      'Pragma': 'no-cache',
-      'Expires': '0'
-    });
-    
+  this.headers = new HttpHeaders({     
+       'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      });   
+      this.options = {headers: this.headers }
    }
 
-   public find(query?: {}): any {
-    const  params = new  HttpParams().set('page', "1").set('size', "1");
-   
-    return this._http.get <PartialList<T>>('http://localhost:8000/api/roles',{params});
+   public find(query?: {}): any {    
+    if (query) {
+      this.options.params = query;
+    } else {
+      this.options.params = undefined;
+    }     
+    return this._http.get <PartialList<T>>('http://localhost:8000/api/roles',this.options);
   }
  
 
