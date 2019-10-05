b0ef1c4f4d3eaa340147e3ec3530793f34767f31
moinultm <moinultm@gmail.com>
2019-10-05 22:53:12 +0600

    added lists

--- src/app/components/stock/product/damage-product/list-damage/list-damage.component.ts
+++ src/app/components/stock/product/damage-product/list-damage/list-damage.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { PartialList } from '@models/common/patial-list.model';
+import { Damage } from '@models/stock/damage.model';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
+import { DamageService } from '@services/stock/damage.service';
 
 @Component({
   selector: 'app-list-damage',
@@ -7,9 +14,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListDamageComponent implements OnInit {
 
-  constructor() { }
+  data: PartialList<Damage>;
+  loading: boolean;
+  savingCategory: boolean;
+  deletingCategory: boolean;
+  page = 1;
+  size = 10;
+  form: FormGroup;
+  //selectedCategory: Category;
+
+  
+  constructor(
+    private damageService: DamageService,
+    private _toastr: ToastrService,
+    private modalService: NgbModal,
+    titleService: Title,
+    private _formBuilder: FormBuilder) {
+
+      titleService.setTitle('Stock - Damage List');
+
+     }
 
   ngOnInit() {
+    this.loadData();
   }
+
+  //Loading Data
+  loadData(page?: number): void {
+    this.page = page ? page : 1;
+    this.loading = true;
+    this.damageService.find({
+      page: this.page,
+      size: this.size
+    }).subscribe((res: PartialList<Damage>) => {
+      this.data = res;
+      this.loading = false;
+    });
+  }
+
 
 }
