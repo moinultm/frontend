57b74075d1c6b92521a08bbf9702b2ab3b5edeb4
moinultm <moinultm@gmail.com>
2019-10-11 21:10:10 +0600

    only order llish

--- src/app/components/stock/sell/order-list/order-list.component.ts
+++ src/app/components/stock/sell/order-list/order-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { DatePipe } from '@angular/common';
+import { SellsOrderService } from '@services/stock/sells-order.service';
+import { PartialList } from '@models/common/patial-list.model';
+import { SellsOrder } from '@models/stock/sells-order.model';
 
 @Component({
   selector: 'app-order-list',
@@ -7,9 +12,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderListComponent implements OnInit {
 
-  constructor() { }
+  loading: boolean;
+  savingSles: boolean;
+  deletingSales: boolean;
+  page = 1;
+  size = 10;
+
+  form: FormGroup;
+  data: any;
+
+  date = new FormControl(new Date());
+  serializedDate = new FormControl((new Date()).toISOString());
+
+
+  constructor(
+    private _fb: FormBuilder,
+    private datePipe : DatePipe,
+    private sellsService:SellsOrderService,
+
+  ) { }
 
   ngOnInit() {
+    this.iniForm();
+    this.loadData();
   }
 
+
+  iniForm(){
+    this.form = this._fb.group({
+      fromDate: [  '',  [Validators.nullValidator],],
+      toDate: [  '',  [Validators.nullValidator],]
+    });
+  }
+
+  loadData(page?: number): void {
+    this.page = page ? page : 1;
+    this.loading = true;
+    let formDt = this.datePipe.transform(this.form.get('fromDate').value, 'yyyy-MM-dd');
+    let toDt = this.datePipe.transform(this.form.get('toDate').value, 'yyyy-MM-dd');
+  
+    this.sellsService.find({
+      page: this.page,
+      size: this.size,
+      from:  formDt,
+      to:   toDt
+    }).subscribe((res: PartialList<SellsOrder>) => {
+      this.data = res;
+      console.log();
+      this.loading = false;
+    });
+  }
 }
