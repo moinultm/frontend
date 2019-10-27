89494d7c201c2716512d3aab3af4046682e5a711
moinultm <moinultm@gmail.com>
2019-10-27 21:58:50 +0600

    removed has

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  rest_url: 'http://localhost:8000/api/',
-  web_url: 'http://localhost:8000/',
-  auth_url: 'http://localhost:8000/oauth/',
+  rest_url: 'https://staritbd.com/api/',
+  web_url: 'https://staritbd.com/',
+  auth_url: 'https://staritbd.com/oauth/',
 };
