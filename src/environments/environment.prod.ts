4205d1c64f03bfb2c7694ac4bf6b354c70b3e4a5
moinultm <moinultm@gmail.com>
2020-01-31 09:44:02 +0600

    http fixed images nedd use dynamic logo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,19 @@
 export const environment = {
   production: true,
+  rest_url: 'https://www.api.gmtradeinternational.com/api/',
+  web_url: 'https://www.api.gmtradeinternational.com/',
+  auth_url: 'https://www.api.gmtradeinternational.com/oauth/',
+  uploads_url: 'https://www.api.gmtradeinternational.com/uploads/',
+};
+
+
+/**
+export const environment = {
+  production: true,
   rest_url: 'https://staritbd.com/api/',
   web_url: 'https://staritbd.com/',
   auth_url: 'https://staritbd.com/oauth/',
   uploads_url: 'ttps://staritbd.com/uploads/',
 };
+
+ */
