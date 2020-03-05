09db4c95a240ed4d42985dd63457bc86b726105b
moinultm <moinultm@gmail.com>
2020-03-05 09:54:19 +0600

    repote checkout

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
-export const environment = {
+ export const environment = {
   production: true,
   rest_url: 'https://www.api.gmtradeinternational.com/api/',
   web_url: 'https://www.api.gmtradeinternational.com/',
@@ -7,12 +7,13 @@ export const environment = {
 };
 
 
-/**
+/*
 export const environment = {
   production: true,
-  rest_url: 'https://staritbd.com/api/',
-  web_url: 'https://staritbd.com/',
-  auth_url: 'https://staritbd.com/oauth/',
-  uploads_url: 'ttps://staritbd.com/uploads/',
+  rest_url: 'https://api.staritbd.com/api/',
+  web_url: 'https://api.staritbd.com/',
+  auth_url: 'https://api.staritbd.com/oauth/',
+  uploads_url: 'https://api.staritbd.com/uploads/',
 };
- */
+
+*/
